df5b34f3ee37ef7bcf0856807d837641e0891d51
Sazumi Viki <75471215+SazumiVicky@users.noreply.github.com>
2022-05-08 23:34:12 +0800

    Create alert.css

---
+++ alert.css
@@ -0,0 +1,7 @@
+let showingAlert = false;
+const interval = setInterval(() => {
+    document.title = showingAlert
+    ? 'Sazumi Cloud': '(1) Sazumi Viki';
+
+    showingAlert = !showingAlert;
+}, 1000);
